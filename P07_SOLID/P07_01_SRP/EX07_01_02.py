06e470d421541c6d90753e18a2d2eaeff817b2ab
Gennadiy Koganovich <genakoganovich@gmail.com>
2025-02-16 19:02:18 +0200

    0003: 07 02 open close start

---
+++ P07_SOLID/P07_01_SRP/EX07_01_02.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'Person(name={self.name})'
+
+
+class PersonDB:
+    def save(self, person):
+        print(f'Save the {person} to the database')
+
+
+if __name__ == '__main__':
+    p = Person('John Doe')
+
+    db = PersonDB()
+    db.save(p)
+    
